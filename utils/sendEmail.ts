8678352eecc86f010a7c20b8f139cc493413f2d8
maxeq <maxeq1@gmail.com>
2023-05-05 13:17:08 +0200

    Update sendEmail.ts

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,14 +1,21 @@
 // utils/sendEmail.ts
 import nodemailer from 'nodemailer';
 
+
 const sendEmail = async (options: any) => {
     // Configure the email transport using Gmail SMTP
     const transport = nodemailer.createTransport({
-        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // Enable TLS
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_APP_PASSWORD,
         },
+        tls: {
+            // Enable insecure mode to allow self-signed certificates
+            rejectUnauthorized: false
+        }
     });
 
     // Set up the email options
@@ -21,6 +28,8 @@ const sendEmail = async (options: any) => {
 
     // Send the email
     await transport.sendMail(mailOptions);
+
 };
+
 
 export default sendEmail;
